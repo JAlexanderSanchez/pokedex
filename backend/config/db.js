ba186a8686e038473348dabef87189c248bbfbb0
JAlexanderSanchez <jasanchezc3@uce.edu.ec>
2025-10-30 20:10:13 -0500

    Cambio

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  try {
+    // ⭐ CAMBIO: Se elimina el segundo argumento (el objeto de opciones)
+    const conn = await mongoose.connect(process.env.MONGO_URI); 
 
-    console.log(`MongoDB Conectado: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error de conexión: ${error.message}`);
-    process.exit(1);
-  }
+    console.log(`MongoDB Conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error de conexión: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
